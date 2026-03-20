975e900dafcb3eb66135274204d6a2299d0ac261
nagithube <agalarovgera@mail.ru>
2026-03-20 19:14:01 +0300

    CR добавлен в зависимости

--- r1r/cli/bin/cli.dart
+++ r1r/cli/bin/cli.dart
@@ -1,19 +1,12 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 
-void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage();
-  } else if (arguments.first == 'version') {
-    print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'wikipedia') {
-    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
-  } else {
-    printUsage();
-  }
+void main(List<String> arguments) async {
+  var runner = CommandRunner();
+  await runner.run(arguments);
 }
 
 void printUsage() {
