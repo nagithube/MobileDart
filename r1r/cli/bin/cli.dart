a7f5842bca594f06d354b7272a338e40e9a7c91f
nagithube <agalarovgera@mail.ru>
2026-03-20 18:32:24 +0300

    Обработка польз.ввода, обработка запросов

--- r1r/cli/bin/cli.dart
+++ r1r/cli/bin/cli.dart
@@ -1,3 +1,38 @@
+import 'dart:io';
+
+const version = '0.0.1';
+
 void main(List<String> arguments) {
-  print('Hello,Dart');
+  if (version.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
+}
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title');
+
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
 }
